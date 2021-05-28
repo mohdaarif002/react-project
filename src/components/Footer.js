956875c128caa9265a20bc19ca29d63e9b35e50c
mohd aarif <aarif75375@gmail.com>
2021-05-28 13:14:37 +0530

    Header and Carousel is created

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <div>
+      <nav class="navbar fixed-bottom navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Fixed bottom
+        </a>
+      </nav>
+    </div>
+  );
+}
